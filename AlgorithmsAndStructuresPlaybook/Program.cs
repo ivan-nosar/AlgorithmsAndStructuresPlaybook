401d0a321846ee0e48c07f1d35d332c152b5d288
ivan-nosar <wowgm@yandex.ru>
2024-04-21 23:56:22 +0200

    Add project files.

---
+++ AlgorithmsAndStructuresPlaybook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsAndStructuresPlaybook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

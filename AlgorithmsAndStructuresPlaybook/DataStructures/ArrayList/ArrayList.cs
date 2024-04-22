c346541e380171073e2fc7e42ed753661e0f6a13
ivan-nosar <wowgm@yandex.ru>
2024-04-22 02:14:59 +0200

    Implemented ArrayList (without comments)

---
+++ AlgorithmsAndStructuresPlaybook/DataStructures/ArrayList/ArrayList.cs
@@ -0,0 +1,110 @@
+﻿namespace AlgorithmsAndStructuresPlaybook.DataStructures.ArrayList
+{
+    public class ArrayList
+    {
+        const int DEFAULT_CAPACITY = 10;
+        const int GROWTH_RATE = 2;
+
+        private int[] Storage { get; set; }
+        private int Capacity { get; set; }
+        public int Count { get; private set; }
+
+        public ArrayList(int capacity = DEFAULT_CAPACITY)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Capacity = capacity;
+            Storage = new int[Capacity];
+            Count = 0;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                ValidateIndexInBounds(index);
+
+                return Storage[index];
+            }
+
+            set
+            {
+                ValidateIndexInBounds(index);
+
+                Storage[index] = value;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (Count == Capacity)
+            {
+                GrowStorage();
+            }
+
+            Storage[Count] = value;
+            Count++;
+        }
+
+        public void RemoveAtIndex(int index)
+        {
+            ValidateIndexInBounds(index);
+
+            Count--;
+            for (int i = index; i < Count; i++)
+            {
+                Storage[i] = Storage[i + 1];
+            }
+
+            // Short-handed form of loop above:
+            // Array.Copy(Storage, index + 1, Storage, index, Count - index);
+        }
+
+        public void InsertAtIndex(int index, int value)
+        {
+            ValidateIndexInBounds(index);
+
+            if (Count == Capacity)
+            {
+                GrowStorage();
+            }
+
+            Count++;
+            for (int i = Count - 1; i > index; i--)
+            {
+                Storage[i] = Storage[i - 1];
+            }
+
+            Storage[index] = value;
+        }
+
+        public void ShrinkToFit()
+        {
+            var oldStorage = Storage;
+            Capacity = Count;
+            Storage = new int[Capacity];
+
+            Array.Copy(oldStorage, Storage, Capacity);
+        }
+
+        private void ValidateIndexInBounds(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        private void GrowStorage()
+        {
+            var oldStorage = Storage;
+            Capacity *= GROWTH_RATE;
+            Storage = new int[Capacity];
+
+            Array.Copy(oldStorage, Storage, oldStorage.Length);
+        }
+    }
+}
--- AlgorithmsAndStructuresPlaybook/Program.cs
+++ AlgorithmsAndStructuresPlaybook/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace AlgorithmsAndStructuresPlaybook
+﻿using AlgorithmsAndStructuresPlaybook.DataStructures.ArrayList;
+
+namespace AlgorithmsAndStructuresPlaybook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var list = new ArrayList(2);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+
+            list.RemoveAtIndex(0); // first index
+            list.RemoveAtIndex(2); // middle index
+            list.RemoveAtIndex(3); // last index
+            list.Add(9);
+
+            list.InsertAtIndex(0, 1); // first index
+            list.InsertAtIndex(3, 4); // middle index
+            list.InsertAtIndex(5, 6); // last index
+            list.InsertAtIndex(6, 7); // last index
+            list.InsertAtIndex(7, 8); // last index
+            list.ShrinkToFit();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
         }
     }
 }

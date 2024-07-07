66a5ee8fc845921fa431cbd0c6afd4b8a6b9ff20
ivan-nosar <wowgm@yandex.ru>
2024-07-07 23:21:26 +0200

    Add unit tests: initial commit

---
+++ AlgorithmsAndStructuresPlaybookTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AlgorithmsAndStructuresPlaybookTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
